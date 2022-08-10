a53d2a104e3e9f0b916663a2e821f58dba3fc797
Saviour <owolabisaviour@gmail.com>
2022-08-10 15:08:10 +0100

    Update recompile script to compile UART_RX

--- uart/vhdl/recompile.sh
+++ uart/vhdl/recompile.sh
@@ -5,6 +5,10 @@ ghdl -a uart_tx.vhd
 ghdl -e uart_tx
 ghdl -r uart_tx
 
+ghdl -a uart_rx.vhd
+ghdl -e uart_rx
+ghdl -r uart_rx
+
 ghdl -a TB_uart.vhd
 ghdl -e TB_uart
 ghdl -r TB_uart --wave=uart.ghw
